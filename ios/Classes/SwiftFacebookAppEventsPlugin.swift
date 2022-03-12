27a7dfaca81a18ed7e7493b25ff8d1ca46f16c38
Sandaruwan Gunathilake <sandaruwan@medicaljoyworks.com>
2022-03-12 11:33:04 +0530

    Added app delegates

--- ios/Classes/SwiftFacebookAppEventsPlugin.swift
+++ ios/Classes/SwiftFacebookAppEventsPlugin.swift
@@ -15,7 +15,29 @@ public class SwiftFacebookAppEventsPlugin: NSObject, FlutterPlugin {
         ApplicationDelegate.shared.initializeSDK()
 
         registrar.addMethodCallDelegate(instance, channel: channel)
+        registrar.addApplicationDelegate(instance)
     }
+
+    // From : https://github.com/darwin-morocho/flutter-facebook-auth/blob/master/facebook_auth/ios/Classes/SwiftFlutterFacebookAuthPlugin.swift
+     /// START ALLOW HANDLE NATIVE FACEBOOK APP
+    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable : Any] = [:]) -> Bool {
+        var options = [UIApplication.LaunchOptionsKey: Any]()
+        for (k, value) in launchOptions {
+            let key = k as! UIApplication.LaunchOptionsKey
+            options[key] = value
+        }
+        ApplicationDelegate.shared.application(application,didFinishLaunchingWithOptions: options)
+        return true
+    }
+
+    public func application( _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:] ) -> Bool {
+        let processed = ApplicationDelegate.shared.application(
+            app, open: url,
+            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+            annotation: options[UIApplication.OpenURLOptionsKey.annotation])
+        return processed;
+    }
+    /// END ALLOW HANDLE NATIVE FACEBOOK APP
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
